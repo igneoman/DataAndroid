82918aec52fdd23a90aa776d0fa3b44418589812
igneoman <gasconignacio@gmail.com>
2020-05-24 00:07:22 +0200

    Correcciones y nuevas actividades para los proveedores.

---
+++ app/src/main/java/com/example/gestor/AgregarProveedor.java
@@ -0,0 +1,14 @@
+package com.example.gestor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarProveedor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_proveedor);
+    }
+}
