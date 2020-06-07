5dc057797fb57342d4d1f8f4f771c3b1e5e6e4d2
igneoman <gasconignacio@gmail.com>
2020-06-07 19:48:31 +0200

    Función eliminar pedido y otros cambios.
    Errores incomprensibles.

--- app/src/main/java/com/example/gestor/AgregarPedido.java
+++ app/src/main/java/com/example/gestor/AgregarPedido.java
@@ -17,39 +17,49 @@ public class AgregarPedido extends AppCompatActivity {
 
     Connection conexion=null;
     PreparedStatement preparedStatement=null;
-    //ResultSet resultSet=null;
 
-    EditText Cif = (EditText) findViewById(R.id.inCif);
-    EditText RSP = (EditText) findViewById(R.id.inRazS);
-    EditText NumF = (EditText) findViewById(R.id.inNumf);
-    EditText DesF = (EditText) findViewById(R.id.inDesf);
-    EditText BasI = (EditText) findViewById(R.id.CodRespC);
-    EditText IVA = (EditText) findViewById(R.id.ImSeg);
-    EditText TotI = (EditText) findViewById(R.id.inTotimp);
-    EditText FeF = (EditText) findViewById(R.id.inFecHom);
-    EditText FeV = (EditText) findViewById(R.id.inFechv);
+    EditText Cif;
+    EditText RSP;
+    EditText NumF;
+    EditText DesF;
+    EditText BasI;
+    EditText IVA;
+    EditText TotI;
+    EditText FeF;
+    EditText FeV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_pedido);
+        Cif = (EditText) findViewById(R.id.inCif);
+        RSP = (EditText) findViewById(R.id.inRazS);
+        NumF = (EditText) findViewById(R.id.inNumf);
+        DesF = (EditText) findViewById(R.id.inDesf);
+        BasI = (EditText) findViewById(R.id.CodRespC);
+        IVA = (EditText) findViewById(R.id.ImSeg);
+        TotI = (EditText) findViewById(R.id.inTotimp);
+        FeF = (EditText) findViewById(R.id.inFecHom);
+        FeV = (EditText) findViewById(R.id.inFechv);
     }
 
     public void ape(View view){
 
-        try {
-            conexion = Connect.conecta();
-            preparedStatement = conexion.prepareStatement("Insert into act_prov VALUES (?,?,?,?,?,?,?,?,?)");
-            preparedStatement.setString(1, String.valueOf(Cif.getText()));
-            preparedStatement.setString(2, Fix.fixdatastr(String.valueOf(RSP.getText())));
-            preparedStatement.setInt(3, Fix.fixdataint(String.valueOf(NumF.getText())));//Str
-            preparedStatement.setString(4, Fix.fixdatastr(String.valueOf(DesF.getText())));
-            preparedStatement.setString(5, Fix.fixdatastr(String.valueOf(BasI.getText())));
-            preparedStatement.setInt(6, Fix.fixdataint(String.valueOf(IVA.getText())));//Str
-            preparedStatement.setInt(7, Fix.fixdataint(String.valueOf(TotI.getText())));//Str
-            preparedStatement.setString(8, Fix.fixdatastr(String.valueOf(FeF.getText())));
-            preparedStatement.setString(9, Fix.fixdatastr(String.valueOf(FeV.getText())));
+       try {
+           conexion = Connect.conecta();
+           if (conexion != null) {
+               preparedStatement = conexion.prepareStatement("Insert into act_prov VALUES (?,?,?,?,?,?,?,?,?)");
 
+               preparedStatement.setString(1, String.valueOf(Cif.getText()));
+               preparedStatement.setString(2, Fix.fixdatastr(String.valueOf(RSP.getText())));
+               preparedStatement.setInt(3, Fix.fixdataint(String.valueOf(NumF.getText())));//Str
+               preparedStatement.setString(4, Fix.fixdatastr(String.valueOf(DesF.getText())));
+               preparedStatement.setString(5, Fix.fixdatastr(String.valueOf(BasI.getText())));
+               preparedStatement.setInt(6, Fix.fixdataint(String.valueOf(IVA.getText())));//Str
+               preparedStatement.setInt(7, Fix.fixdataint(String.valueOf(TotI.getText())));//Str
+               preparedStatement.setString(8, Fix.fixdatastr(String.valueOf(FeF.getText())));
+               preparedStatement.setString(9, Fix.fixdatastr(String.valueOf(FeV.getText())));
+           }
 
             int ok = preparedStatement.executeUpdate();
             if (ok > 0) {
@@ -65,6 +75,8 @@ public class AgregarPedido extends AppCompatActivity {
         } catch (SQLException e1) {
             e1.printStackTrace();
         }
+
+        finish();
 
     }
 
--- app/src/main/java/com/example/gestor/EliminarPedido.java
+++ app/src/main/java/com/example/gestor/EliminarPedido.java
@@ -4,13 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class EliminarPedido extends AppCompatActivity {
+
+    Connection conexion=null;
+    PreparedStatement preparedStatement=null;
+
+    EditText Cif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eliminar_pedido);
+        Cif = (EditText) findViewById(R.id.inNumf2);
+    }
+
+    public void ElP(View view){
+
+        try {
+            conexion = Connect.conecta();
+            preparedStatement = conexion.prepareStatement("Delete from act_prov where NUM_FACTURA=?");
+            preparedStatement.setString(1, String.valueOf(Cif.getText()));
+            int ok = preparedStatement.executeUpdate();
+            if (ok > 0) {
+                Toast.makeText(getApplicationContext(),"Dato eliminado", Toast.LENGTH_SHORT).show();
+                conexion.close();
+            }
+            else {
+                if(Cif.getText().equals("")) {
+                    Toast.makeText(getApplicationContext(),"Error, valor nulo", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Error, valor inexistente", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+
     }
 
     public void canp(View view){
