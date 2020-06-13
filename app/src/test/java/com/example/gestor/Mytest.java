3bd1b906445042c8e02e4441a59a13d15ffeca6f
igneoman <gasconignacio@gmail.com>
2020-06-13 23:19:15 +0200

    Agregación de mi test para mi app android.

---
+++ app/src/test/java/com/example/gestor/Mytest.java
@@ -0,0 +1,17 @@
+package com.example.gestor;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class Mytest {
+    @Test
+    public void TesConnect() {
+        com.example.gestor.Connect.conecta();
+    }
+}
